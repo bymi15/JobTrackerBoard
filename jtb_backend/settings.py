b18f10704bc43ba09cac2f427a1e76bdeb10e460
unknown <bymi15@yahoo.com>
2020-01-03 17:37:42 +0000

    .

--- jtb_backend/settings.py
+++ jtb_backend/settings.py
@@ -150,6 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 

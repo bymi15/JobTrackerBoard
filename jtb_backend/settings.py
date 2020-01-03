83313455d0e092b33ff06cba927b6142c9c53efb
unknown <bymi15@yahoo.com>
2020-01-03 17:19:06 +0000

    updated

--- jtb_backend/settings.py
+++ jtb_backend/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'jtb_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

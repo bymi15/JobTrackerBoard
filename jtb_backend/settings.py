4f1b0df308d674a7e98a26a8d8317da7188d6d25
unknown <bymi15@yahoo.com>
2020-01-03 18:38:08 +0000

    .

--- jtb_backend/settings.py
+++ jtb_backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
